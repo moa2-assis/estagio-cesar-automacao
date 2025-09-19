c450eacf93d6e86b79076b81b9a850f17d3d8e27
moa2-assis <moa2@cesar.or.br>
2025-09-19 08:59:39 -0300

    Feature: conftest for fixtures (Setup/Teardown)

---
+++ conftest.py
@@ -0,0 +1,13 @@
+# conftest.py
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    # Setup: initialize the WebDriver
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+
+    # Teardown: close the WebDriver
+    driver.quit()
